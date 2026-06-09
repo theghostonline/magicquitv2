3752eba5412c0feaa48b79adeec212915af320ad
Husam Osman <husamosman28@outlook.com>
2026-06-09 16:23:27 +0100

    Clean up cask URL stanza

--- Casks/magicquit.rb
+++ Casks/magicquit.rb
@@ -2,8 +2,7 @@ cask "magicquit" do
   version "1.4.1"
   sha256 "acd091dcde3100a732d6fca8f1825a9baf771bace753c02a5e4c5e3899cec29a"
 
-  url "https://github.com/theghostonline/magicquitv2/releases/download/v#{version}/MagicQuit-#{version}.zip",
-      verified: "github.com/theghostonline/magicquitv2/"
+  url "https://github.com/theghostonline/magicquitv2/releases/download/v#{version}/MagicQuit-#{version}.zip"
   name "MagicQuit"
   desc "Menu bar app that quits inactive apps with app exclusions"
   homepage "https://github.com/theghostonline/magicquitv2"
